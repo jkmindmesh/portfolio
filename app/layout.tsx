3be7912c3b6e3130a06a380a95d076aaee604ad9
jkmindmesh <jk.mindmesh@gmail.com>
2026-04-05 19:35:20 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+```tsx
+import "./globals.css";
+import Navbar from "../components/Navbar";
+
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
