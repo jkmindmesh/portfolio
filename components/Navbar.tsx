3ab5b4719e1f0a13925f133d750408f3fd1c9088
jkmindmesh <jk.mindmesh@gmail.com>
2026-04-05 19:43:40 -0400

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+## components/Navbar.tsx
+```tsx
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-6 border-b border-zinc-800">
+      <h1 className="font-bold text-xl">Your Name</h1>
+      <div className="flex gap-6 text-gray-400">
+        <Link href="/projects">Projects</Link>
+        <Link href="/experiences">Experiences</Link>
+        <Link href="/activities">Activities</Link>
+      </div>
+    </nav>
+  );
+}
