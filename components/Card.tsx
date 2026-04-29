b57c6a133245c9ade48a125cda1d1097eed6e1c0
jkmindmesh <jk.mindmesh@gmail.com>
2026-04-28 21:29:09 -0400

    Enhance Card component with url prop and Link
    Added 'url' prop to Card component and wrapped content in a Link.

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,8 +3,9 @@ interface CardProps {
   description: string;
 }
 
-export default function Card({ title, description }: CardProps) {
+export default function Card({ title, description, url }: CardProps) {
   return (
+    <Link href={url} target="_blank">
     <div className="group p-8 bg-white border border-blue-100 rounded-3xl shadow-sm hover:shadow-md hover:border-blue-300 transition-all duration-300">
       <div className="w-12 h-1 bg-blue-500 mb-4 rounded-full opacity-20 group-hover:opacity-100 transition-opacity" />
       <h2 className="text-2xl font-bold text-slate-800 mb-2">
@@ -14,5 +15,6 @@ export default function Card({ title, description }: CardProps) {
         {description}
       </p>
     </div>
+      </Link>
   );
 }
